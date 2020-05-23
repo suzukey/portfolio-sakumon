59d648a448b20ce554e2ac1bb766f469c5af0c79
suzukey <suzukei0208@gmail.com>
2020-05-23 23:39:59 +0900

    [add] 投稿機能の追加

--- backend/app/db/schema.rb
+++ backend/app/db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_17_163614) do
+ActiveRecord::Schema.define(version: 2020_05_21_094352) do
+
+  create_table "posts", force: :cascade do |t|
+    t.integer "user_id"
+    t.string "title"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "provider", default: "email", null: false
