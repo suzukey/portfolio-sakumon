25a200c362ccb38506cd2ac9992a1b8d63e3b1df
suzukey <suzukei0208@gmail.com>
2020-06-17 23:44:34 +0900

    [add] ユーザーの投稿を取得するController, routeの作成

--- backend/app/app/controllers/v1/user_posts_controller.rb
+++ backend/app/app/controllers/v1/user_posts_controller.rb
@@ -1,4 +1,15 @@
 module V1
   class UserPostsController < ApplicationController
+    def index
+      if v1_user_signed_in? && current_v1_user.name == params[:user_name]
+        posts = current_v1_user.posts
+      else
+        posts = User.find_by!(name: params[:user_name])
+                    .posts
+                    .status_public
+      end
+
+      render json: posts, status: :ok
+    end
   end
 end
