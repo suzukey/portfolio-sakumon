c6402ffab02470692fb1b09ce6530e18e60ac5f4
suzukey <suzukei0208@gmail.com>
2020-06-25 23:12:19 +0900

    [add] Headerの追加

---
+++ frontend/app/components/Header.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-app-bar fixed color="cyan darken-2" dark>
+    <v-app-bar-nav-icon />
+    <v-toolbar-title>Sakumon</v-toolbar-title>
+
+    <v-spacer />
+
+    <v-btn icon>
+      <v-icon>mdi-magnify</v-icon>
+    </v-btn>
+
+    <v-btn icon>
+      <v-icon>mdi-account-circle</v-icon>
+    </v-btn>
+  </v-app-bar>
+</template>
