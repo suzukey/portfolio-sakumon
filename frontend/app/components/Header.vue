393eca2c9519b8893f5f071592559b1aa4aad520
suzukey <suzukei0208@gmail.com>
2020-06-28 00:46:42 +0900

    [change] レスポンシブヘッダー

--- frontend/app/components/Header.vue
+++ frontend/app/components/Header.vue
@@ -1,21 +1,45 @@
 <template>
   <v-app-bar app color="cyan darken-2" dark>
-    <v-toolbar-title>SakuMon</v-toolbar-title>
+    <v-toolbar-title class="d-none d-sm-flex">
+      SakuMon
+    </v-toolbar-title>
 
     <v-spacer />
 
-    <v-btn icon>
-      <v-icon>mdi-magnify</v-icon>
-    </v-btn>
+    <div v-if="isAuthenticated">
+      <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
 
-    <v-btn icon>
-      <v-icon>mdi-square-edit-outline</v-icon>
-    </v-btn>
+      <v-btn icon>
+        <v-icon>mdi-square-edit-outline</v-icon>
+      </v-btn>
 
-    <v-btn icon>
-      <v-avatar color="" size="48">
-        <v-icon dark>mdi-account-circle</v-icon>
-      </v-avatar>
-    </v-btn>
+      <v-btn icon>
+        <v-avatar color="" size="48">
+          <v-icon dark>mdi-account-circle</v-icon>
+        </v-avatar>
+      </v-btn>
+    </div>
+    <div v-else>
+      <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+
+      <v-btn text>ログイン</v-btn>
+
+      <v-btn outlined>新規登録</v-btn>
+    </div>
   </v-app-bar>
 </template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  computed: {
+    isAuthenticated () {
+      return Math.random() >= 0.5
+    }
+  }
+})
+</script>
