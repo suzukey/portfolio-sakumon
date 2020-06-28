28842c3549add71dd5f7426e5b0a5874504a6913
suzukey <suzukei0208@gmail.com>
2020-06-28 21:36:12 +0900

    [add] エラーページの見た目を作成

--- frontend/app/layouts/error.vue
+++ frontend/app/layouts/error.vue
@@ -1,20 +1,29 @@
 <template>
   <v-main>
-    <h1 v-if="error.statusCode === 404">
-      {{ pageNotFound }}
-    </h1>
-    <h1 v-else>
-      {{ otherError }}
-    </h1>
-    <NuxtLink to="/">
-      Home page
-    </NuxtLink>
+    <v-container>
+      <v-row justify="center">
+        <v-col cols="12" sm="10" md="8" lg="6">
+          <v-card>
+            <v-card-title>エラー</v-card-title>
+            <v-card-subtitle v-if="error.statusCode === 404">
+              {{ pageNotFound }}
+            </v-card-subtitle>
+            <v-card-subtitle v-else>
+              {{ otherError }}
+            </v-card-subtitle>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn text nuxt to="/">ホームに戻る</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
   </v-main>
 </template>
 
 <script>
 export default {
-  layout: 'error',
   props: {
     error: {
       type: Object,
