baeed3db22ad1874ca1b66576a32c32c78b4383b
suzukey <suzukei0208@gmail.com>
2020-06-30 20:31:35 +0900

    [change] ダークモードprimary, accentカラーの調整

--- frontend/app/nuxt.config.js
+++ frontend/app/nuxt.config.js
@@ -72,11 +72,11 @@ export default {
   vuetify: {
     customVariables: ['~/assets/variables.scss'],
     theme: {
-      dark: false,
+      dark: true,
       themes: {
         dark: {
-          primary: colors.cyan.darken2,
-          accent: colors.amber.darken2,
+          primary: colors.cyan.darken3,
+          accent: colors.amber.darken3,
           secondary: colors.amber.darken3,
           info: colors.teal.lighten1,
           warning: colors.amber.base,
@@ -91,7 +91,7 @@ export default {
           warning: colors.amber.base,
           error: colors.deepOrange.accent4,
           success: colors.green.accent3,
-        }
+        },
       },
     },
   },
