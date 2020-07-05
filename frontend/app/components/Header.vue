1776672c96985a722679bb54b6a688b914bf9342
suzukey <suzukei0208@gmail.com>
2020-07-06 01:00:55 +0900

    [add] ヘッダーに検索バーを追加

--- frontend/app/components/Header.vue
+++ frontend/app/components/Header.vue
@@ -1,28 +1,51 @@
 <template>
-  <v-app-bar app color="primary" dark :elevate-on-scroll="elevateOnScroll">
-    <v-toolbar-title class="d-none d-sm-flex">
-      <nuxt-link to="/" class="title-link font-weight-medium">
-        SakuMon
-      </nuxt-link>
-    </v-toolbar-title>
+  <v-app-bar
+    v-click-outside="onClickOutside"
+    app
+    color="primary"
+    dark
+    :elevate-on-scroll="elevateOnScroll"
+  >
+    <template v-if="!search">
+      <v-toolbar-title class="d-none d-sm-flex">
+        <nuxt-link to="/" class="title-link font-weight-medium">
+          SakuMon
+        </nuxt-link>
+      </v-toolbar-title>
 
-    <v-spacer />
+      <v-spacer />
 
-    <v-btn icon>
-      <v-icon>mdi-magnify</v-icon>
-    </v-btn>
-
-    <template v-if="isAuthenticated">
-      <v-btn icon>
-        <v-icon>mdi-square-edit-outline</v-icon>
+      <v-btn icon @click="search = true">
+        <v-icon>mdi-magnify</v-icon>
       </v-btn>
 
-      <account-menu></account-menu>
+      <template v-if="isAuthenticated">
+        <v-btn icon>
+          <v-icon>mdi-square-edit-outline</v-icon>
+        </v-btn>
+
+        <account-menu></account-menu>
+      </template>
+      <template v-else>
+        <v-btn text nuxt to="/login">ログイン</v-btn>
+
+        <v-btn outlined nuxt to="/register">新規登録</v-btn>
+      </template>
     </template>
     <template v-else>
-      <v-btn text nuxt to="/login">ログイン</v-btn>
-
-      <v-btn outlined nuxt to="/register">新規登録</v-btn>
+      <v-btn icon class="mr-2" @click="search = false">
+        <v-icon>mdi-arrow-left</v-icon>
+      </v-btn>
+      <v-text-field
+        autofocus
+        clearable
+        dense
+        hide-details
+        outlined
+        single-line
+        prepend-inner-icon="mdi-magnify"
+        label="検索"
+      ></v-text-field>
     </template>
   </v-app-bar>
 </template>
@@ -40,9 +63,19 @@ export default {
       default: false,
     },
   },
+  data() {
+    return {
+      search: false,
+    }
+  },
   computed: {
     isAuthenticated() {
       return false
+    },
+  },
+  methods: {
+    onClickOutside() {
+      this.search = false
     },
   },
 }
