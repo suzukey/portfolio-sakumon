066bc0b10f01f1cba2a64674d689cf9df350086f
suzukey <suzukei0208@gmail.com>
2020-08-03 03:23:01 +0900

    [add] ログイン・新規登録ページの完成

--- frontend/app/pages/login.vue
+++ frontend/app/pages/login.vue
@@ -1,43 +1,43 @@
 <template>
   <v-card-text>
     <page-selector active-page="login"></page-selector>
-    <v-form @submit.prevent="submit">
-      <v-text-field
-        v-model="email"
-        prepend-inner-icon="mdi-email"
-        label="メールアドレス"
-      />
-      <v-text-field
-        v-model="password"
-        type="password"
-        prepend-inner-icon="mdi-lock"
-        label="パスワード"
-      />
-      <v-checkbox
-        v-model="autoLogin"
-        hide-details
-        class="mt-7 mb-5"
-        label="ログインしたままにする"
-      ></v-checkbox>
-      <v-btn
-        type="submit"
-        block
-        color="primary"
-        depressed
-        x-large
-        class="mb-0 mt-2"
-      >
-        <span class="font-weight-bold text-subtitle-1">ログイン</span>
-      </v-btn>
-      <easy-login />
-      <p class="forgot text-right mb-0 mt-5">
-        <nuxt-link to="forgot-password">パスワードを忘れた場合</nuxt-link>
-      </p>
-    </v-form>
+    <v-text-field
+      v-model="credential.email"
+      prepend-inner-icon="mdi-email"
+      label="メールアドレス"
+    />
+    <v-text-field
+      v-model="credential.password"
+      type="password"
+      prepend-inner-icon="mdi-lock"
+      label="パスワード"
+    />
+    <v-checkbox
+      v-model="autoLogin"
+      hide-details
+      class="mt-7 mb-5"
+      label="ログインしたままにする"
+    ></v-checkbox>
+    <v-btn
+      block
+      color="primary"
+      depressed
+      x-large
+      class="mb-0 mt-2"
+      @click="loginProcess"
+    >
+      <span class="font-weight-bold text-subtitle-1">ログイン</span>
+    </v-btn>
+    <easy-login />
+    <p class="forgot text-right mb-0 mt-5">
+      <nuxt-link to="forgot-password">パスワードを忘れた場合</nuxt-link>
+    </p>
   </v-card-text>
 </template>
 
 <script>
+import { mapActions } from 'vuex'
+
 import PageSelector from '~/components/auth/PageSelector.vue'
 import EasyLogin from '~/components/auth/EasyLogin.vue'
 
@@ -48,10 +48,19 @@ export default {
   },
   data() {
     return {
-      email: '',
-      password: '',
+      credential: {
+        email: '',
+        password: '',
+      },
+
       autoLogin: false,
     }
+  },
+  methods: {
+    async loginProcess() {
+      await this.login(this.credential)
+    },
+    ...mapActions('auth', ['login']),
   },
   head() {
     return {
